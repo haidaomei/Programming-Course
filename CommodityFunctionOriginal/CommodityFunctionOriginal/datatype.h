f5bb189d97ff10769b5c0d6b152e33cf72c3466b
haidaomei <DDdaomei@hotmail.com>
2025-07-27 09:51:37 +0800

    Add files via upload

---
+++ CommodityFunctionOriginal/CommodityFunctionOriginal/datatype.h
@@ -0,0 +1,13 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+#include <string>
+struct CommodityInfo
+{
+    long id;
+    std::string name;
+    double price;
+    int num;
+    double discount;
+};
+const int MAX_COMMODITY_NUM=100;
+#endif // DATATYPE_H
---
+++ CommodityFunctionOriginal/CommodityFunctionOriginal/function.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+char menu();
+void displayCommodities(CommodityInfo* pCommodities, int num);
+void addCommodity(CommodityInfo* pCommodities, int &num);
+void removeCommodity(CommodityInfo* pCommodities, int &num);
+void viewCommodity(CommodityInfo* pCommodities, int num);
+void checkOut(CommodityInfo* pCommodities, int num);
+void readData(std::string filename);
+void writeData(std::string filename);
+#endif // FUNCTION_H
